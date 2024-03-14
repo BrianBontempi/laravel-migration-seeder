b610926fde4a180097b31a6e5e63c13b03e49cc0
BrianBontempi <brian.bont54@gmail.com>
2024-03-14 16:21:09 +0100

    fix and add readme

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',);
-Route::get('/home', HomeController::class)->name('home');
+Route::get('/', HomeController::class)->name('home');
 
 Route::get('/trains', TrainController::class)->name('train');
